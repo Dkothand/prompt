25e0a871f6bb401a75ed84b3cc26f9afa180f9fc
Dkothand <dkothand@gmail.com>
2021-11-15 17:28:15 -0500

    Initial commit for tests

--- selection/app/tests.py
+++ selection/app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from app.models import Provider
 
-# Create your tests here.
+class ProviderTestCase(TestCase):
+    def setUp(self):
+        Provider.objects.create(name="Test Provider")
+
+    def test_defaults(self):
+        """Providers defaults"""
+        test_provider = Provider.objects.get(name="Test Provider")
+        self.assertEqual(test_provider.fees, 0.0000)
+        self.assertEqual(test_provider.minimum_balance, 0.0000)
+        self.assertEqual(test_provider.automated, False)
+        self.assertEqual(test_provider.advisor, False)
+        self.assertEqual(test_provider.ease_of_use, 3)
+
